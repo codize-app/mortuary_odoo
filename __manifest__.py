50b1bc250a6ddda25970bf3a9ffc61b756923d7e
Ignacio Buioli <ibuioli@gmail.com>
2022-01-10 15:19:28 -0300

    feat: initial commit

--- __manifest__.py
+++ __manifest__.py
@@ -1,17 +1,16 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "odoo_package_addon",
+    'name': "Odoo for Mortuary business",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Odoo for Mortuary business""",
 
     'description': """
-        Long description of module's purpose
+        Odoo for Mortuary business
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Codize",
+    'website': "https://www.codize.ar",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
@@ -24,12 +23,8 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        'views/res_partner.xml',
     ],
     # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+    'demo': [],
 }
